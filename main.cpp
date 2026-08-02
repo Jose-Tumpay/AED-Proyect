943e8cf812f7b9699ada92366bdac1c7382b46f0
Jose-Tumpay <jtumpay@unsa.edu.pe>
2026-08-01 22:56:47 -0500

    agragmos la primera fucnion al main, pd si funciono

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 #include "red/redSocial.h"
+
+int main() {
+    RedSocial red;
+    red.cargarGrafoSNAP("data/amistades_4039n_88234r.txt");
+    std::cout << red.obtenerNumUsuarios() << std::endl;
+    return 0;
+}
